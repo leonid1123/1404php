7e16719f8cbe1dcb9b526ee06d3109a87ed85385
Leo <47742726+leonid1123@users.noreply.github.com>
2022-09-20 11:53:16 +0300

    pics

--- index.php
+++ index.php
@@ -1,44 +1,42 @@
-<?php
-if (isset($_POST["login"])) {
-    $login = $_POST["login"];
-    $password = $_POST["pass"];
-}
-?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP</title>
+    <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
-    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" method="post">
-        <p>Login</p>
-        <input type="text" name="login">
-        <p>Password</p>
-        <input type="text" name="pass">
+    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
+        <p>Выберите картинку</p>
+        <select name="pictures">
+            <option value="smile1">Смайлик 1</option>
+            <option value="smile2">Смайлик 2</option>
+        </select>
+        <p>Выберите размер картинки по ширине</p>
+        <select name="picSize">
+            <option value="small">маленький</option>
+            <option value="medium">средний</option>
+            <option value="big">большой</option>
+        </select>
         <input type="submit" value="ТЫК!">
     </form>
-
     <?php
-    if (isset($login)&isset($password)) {
-        $txt1="Вы ввели логин:";
-        $txt2="Вы ввели пароль:";
-        $smile1 = "01.png";
-        $smile2 = "02.png";
-        echo "<p>".$txt1." ".$login."</p>";
-        echo "<p>".$txt2." ".$password."</p>";
-        if ($login==$password) {
-            echo "<p>Ну так совсем не надо 8-/ </p>" ;
-            echo "<img src=".$smile1.">";
-        } else if ($login=="") {
-            echo "<p>Пустой логин плохая идея</p>";
-            echo "<img src=".$smile2.">";
-        } else if ($password=="123456") {
-            echo "Это кем надо быть, чтобы придумать такой пароль 0_о";
+        $selectedPic="";
+        $smile1="01";
+        $smile2="02";
+        $pic = $_POST["pictures"];
+        $picSize = $_POST["picSize"];
+        if ($pic=="smile1" ) {
+            $selectedPic = $smile1;
+        } else if($pic=="smile2") {
+            $selectedPic = $smile2;
         }
-    }
-    ?>
+        ?>
+    <img src= <?php echo "'". $selectedPic.".png'"; ?> >
 </body>
+
 </html>
