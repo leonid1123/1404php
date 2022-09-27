4f60fb16a4773d8b9a90ca04d9889cd92b364d0e
Leo <47742726+leonid1123@users.noreply.github.com>
2022-09-27 11:49:41 +0300

    file upload

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,7 +7,6 @@
     <title>PHP</title>
     <link rel="stylesheet" href="style.css">
 </head>
-
 <body>
     <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
         <p>Выберите картинку</p>
@@ -18,25 +16,34 @@
         </select>
         <p>Выберите размер картинки по ширине</p>
         <select name="picSize">
-            <option value="small">маленький</option>
-            <option value="medium">средний</option>
-            <option value="big">большой</option>
+            <option value="smallPic">маленький</option>
+            <option value="mediumPic">средний</option>
+            <option value="bigPic">большой</option>
         </select>
+        <p>Загрузить своё изображение</p>
+        <input type="file" name="file">
         <input type="submit" value="ТЫК!">
     </form>
     <?php
         $selectedPic="";
-        $smile1="01";
-        $smile2="02";
-        $pic = $_POST["pictures"];
-        $picSize = $_POST["picSize"];
-        if ($pic=="smile1" ) {
-            $selectedPic = $smile1;
-        } else if($pic=="smile2") {
-            $selectedPic = $smile2;
-        }
+        $picSize="";
+        $targetDir="upload/";
+        $targetFile = $targetDir . basename($_FILES["file"]["name"]);
+        move_uploaded_file($_FILES["file"]["tmp_name"], $targetFile);
+        //загрузка не работает, не видит загружаемый файл
+        if (isset($_POST["pictures"]) & isset($_POST["picSize"])) {
+            $smile1="01";
+            $smile2="02";
+            $pic = $_POST["pictures"];
+            $picSize = $_POST["picSize"];
+            if ($pic=="smile1" ) {
+                $selectedPic = $smile1;
+            } else if($pic=="smile2") {
+                $selectedPic = $smile2;
+            }
+        }           
         ?>
-    <img src= <?php echo "'". $selectedPic.".png'"; ?> >
+    <img class=<?php echo "'".$picSize."'"; ?> src= <?php echo "'". $selectedPic.".png'"; ?> >
 </body>
 
 </html>
